bb3a5045cf7c52043fae71219f856eca35d33c84
Akifcan <akfkara97@gmail.com>
2024-08-12 02:29:05 +0300

    fix(hook): removed unused line

--- hooks/user.hook.ts
+++ hooks/user.hook.ts
@@ -1,17 +1,8 @@
 import { useAppSelector } from '@/store/store'
 import Cookies from 'js-cookie'
-import { useEffect, useRef, useState } from 'react'
 
 export const useUser = () => {
   const currency = useAppSelector((state) => state.currency.currency)
-  const [visitorId, setVisitorId] = useState<number>()
-
-  useEffect(() => {
-    if (!Cookies.get('VISITOR_ID')) {
-      Cookies.set('VISITOR_ID', `${Math.floor(Math.random() * 99999)}`)
-    }
-    setVisitorId(Number(Cookies.get('VISITOR_ID')))
-  }, [])
 
   const handleVisitorId = () => {
     if (!Cookies.get('VISITOR_ID')) {

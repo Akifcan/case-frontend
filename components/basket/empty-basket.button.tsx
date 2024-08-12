b1e647e59623f254795ecd631743530b4fafdedb
Akifcan <akfkara97@gmail.com>
2024-08-12 21:55:58 +0300

    feat(basket): added empty basket button

--- components/basket/empty-basket.button.tsx
+++ components/basket/empty-basket.button.tsx
@@ -1,15 +1,46 @@
 import { useState } from 'react'
 import Dialog from '../dialog/dialog'
+import { useUser } from '@/hooks/user.hook'
+import fetcher from '@/store/fetcher'
+import toast, { Toaster } from 'react-hot-toast'
+import { useMutation } from '@tanstack/react-query'
+import { queryClient } from '@/store/redux.provider'
 
 export default function EmptyBasket() {
   const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false)
+  const { visitorId } = useUser()
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async () => {
+      return await fetcher<{ message: string; empty: false }>(`/basket/empty`, {
+        method: 'DELETE',
+        body: {
+          visitorId,
+        },
+      })
+    },
+    onSuccess: (data) => {
+      toast(data.message, { position: 'top-right' })
+      if (data.empty) {
+        queryClient.fetchQuery({ queryKey: ['basket'] })
+      }
+    },
+    onError: () => {
+      toast('Beklenmedik bir hata oluştu', { position: 'top-right' })
+    },
+  })
 
   const handleClose = (confirmed: boolean) => {
     setDeleteDialogOpen(false)
+    if (!confirmed) {
+      return
+    }
+    mutate()
   }
 
   return (
     <>
+      <Toaster />
       <Dialog
         isOpen={isDeleteDialogOpen}
         onClose={handleClose}
@@ -17,6 +48,7 @@ export default function EmptyBasket() {
         subtitle="tüm sepeti temizlemek istiyor musunuz?"
       />
       <button
+        disabled={isPending}
         className="p-half"
         onClick={() => {
           setDeleteDialogOpen(true)
